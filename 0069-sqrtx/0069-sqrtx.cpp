0975d1a17f4a544185307d7b0eec02dafcba4b64
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-14 21:36:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        if(x==1 || x==0)return x;
-       long long low=1;
-       long long high=x/2,ans=-1;
-        while(low<=high){
-            long long mid=(low+high)/2;
-            if((mid*mid)<=x){
-                ans=mid;
-                if((mid*mid)==x){
-                    return ans;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
-            else{
-                high=mid-1;
-            }
+    int mySqrt(int n) {
+    long long low = 1, high = n;
+    //Binary search on the answers:
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+        long long val = mid * mid;
+        if (val <= (long long)(n)) {
+            //eliminate the left half:
+            low = mid + 1;
         }
-        return ans;
+        else {
+            //eliminate the right half:
+            high = mid - 1;
+        }
     }
+    return high;
+    }
+
 };
