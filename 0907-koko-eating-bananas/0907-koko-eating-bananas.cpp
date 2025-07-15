f32889467d199b2ffdbca3284a01739ba7098421
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-16 01:01:55 +0530

    Time: 4 ms (88.85%), Space: 23 MB (41.96%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    long long findhour(int ban, const vector<int>& arr, int n){
-        long long total = 0;
-        for(int i = 0; i < n; i++){
-            total += (arr[i] + ban - 1) / ban; // integer ceil
-        }
-        return total;
-    }
-
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        int mxbn = *max_element(piles.begin(), piles.end());
-        int low = 1, high = mxbn, ans = mxbn;
-
+        long long low=1,high=*max_element(piles.begin(),piles.end()),ans;
         while(low <= high){
-            int mid = low + (high - low) / 2;
-            long long result = findhour(mid, piles, n);
-
+            long long mid = (low + high)/ 2;
+            long long result = 0;
+            for(int i = 0; i < n; i++){
+               result += (piles[i] + mid - 1) / mid; // integer ceil
+            }
             if(result <= h){
                 ans = mid;
                 high = mid - 1;
@@ -25,7 +17,6 @@ public:
                 low = mid + 1;
             }
         }
-
         return ans;
     }
 };
