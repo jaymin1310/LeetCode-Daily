0ead3004849dee54a33b87112c2772c4a9e5cc54
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-17 00:52:17 +0530

    Time: 7 ms (71.03%), Space: 26.2 MB (47.63%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -3,12 +3,11 @@ public:
     int smallestDivisor(vector<int>& nums, int th) {
         //agge min milega
         int l=1,h=*max_element(nums.begin(),nums.end()),mid;
-        int n=nums.size();
         int sum=0,ans=0;
         while(l<=h){
             mid=l+(h-l)/2;
             sum=0;
-            for(int i=0;i<n;i++){
+            for(int i=0;i<nums.size();i++){
                 sum+=(nums[i]+mid-1)/mid;
             }
             if(sum<=th){
