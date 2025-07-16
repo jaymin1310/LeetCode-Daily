70cf81bf1f4e6bddb94f6764b0b416a5225d7476
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-17 00:47:22 +0530

    Time: 11 ms (47.05%), Space: 26.2 MB (16.12%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int checksum(vector<int>&arr,int div,int n){
-        int sum=0;
-        for(int i=0;i<n;i++){
-            sum+=(arr[i]+div-1)/div;
-        }
-        return sum;
+    int sumByD(vector<int> &arr, int div) {
+    int n = arr.size(); //size of array
+    //Find the summation of division values:
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += ceil((double)(arr[i]) / (double)(div));
     }
-    int smallestDivisor(vector<int>& nums, int threshold) {
-        int n=nums.size();
-        int max=*max_element(nums.begin(),nums.end());
-        int low=1,high=max,mid,ans=-1;
-        while(low<=high){
-            mid=(low+high)/2;
-            int result=checksum(nums,mid,n);
-            if(result>threshold){
-                low=mid+1;
-            }
-            else{
-                ans=mid;
-                high=mid-1;
-            }
+    return sum;
+}
+
+int smallestDivisor(vector<int>& arr, int limit) {
+    int n = arr.size();
+    if (n > limit) return -1;
+    int low = 1, high = *max_element(arr.begin(), arr.end());
+
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (sumByD(arr, mid) <= limit) {
+            high = mid - 1;
         }
-    return ans;
+        else {
+            low = mid + 1;
+        }
     }
+    return low;
+}
 };
