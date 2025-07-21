39b6ba61fdf478769bc03704e0f682ebdc163d40
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-21 13:57:45 +0530

    Time: 0 ms (100%), Space: 13.3 MB (26.94%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    bool iselem(vector<int>&arr,int ele,int n){
+    bool iselem(vector<int>&arr,int ele,int n,int &left){
+        int flag=0;
         for(int i=0;i<n;i++){
+            if(arr[i]<ele)left++;
             if(ele==arr[i]){
-                return true;
+                flag=1;
             }
         }
+        left=(ele-1)-left;
+        if(flag==1)return true;
+        left++;
         return false;
     }
     int findKthPositive(vector<int>& arr, int k) {
         int n=arr.size();
         int maxel=*max_element(arr.begin(),arr.end());
         int miss=maxel-n;
-        int high,cnt=0;
+        int low=1,mid,high,cnt=0;
         if(k>miss){
            high=maxel+(k-miss);
         }
         else{
             high=maxel;
         }
-        for(int i=1;i<=high;i++){
-            if(!iselem(arr,i,n)){
-                cnt++;
+        while(low<=high){
+            mid=(low+high)/2;
+            int left=0;
+            if(!iselem(arr,mid,n,left)){
+                if(left==k){
+                    return mid;
+                }
+                else if(left<k){
+                    low=mid+1;
+                }
+                else{
+                   high=mid-1;
+                }
             }
-            if(cnt==k){
-                return i;
+            else{
+                if(left<k){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
             }
         }
         return -1;
