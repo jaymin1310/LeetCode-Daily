496c0f7181b2936bf6e909196453753ca33da2e6
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-21 10:58:47 +0530

    Time: 8 ms (84.74%), Space: 35.1 MB (34.62%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool possible(vector<int>&arr,int wei,int day,int n){
-        int cnt=0,sum=0;
+        int cnt=1,sum=0;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            if(sum>wei && i==n-1){
-                cnt+=2;
-            }
-            else if(sum>wei || i==n-1){
+            if(sum>wei ){
                 sum=arr[i];
                 cnt++;
             }
