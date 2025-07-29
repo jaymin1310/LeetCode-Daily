d7bfb08ee2a698273162a4e3f6504ba1726e75f2
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-30 00:33:19 +0530

    Time: 0 ms (100%), Space: 95 MB (93.19%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        int i=n-1,j=0;
-        while(i>=0 && j<m){
-            if(nums1[i]>nums2[j]){
-                swap(nums1[i],nums2[j]);
-                i--;
-                j++;
+        int n1=nums1.size();
+        int n2=nums2.size();
+        if(n2<n1)return findMedianSortedArrays(nums2,nums1);
+        int low=0,high=n1;
+        int leftTot=(n1+n2+1)/2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int mid1=mid;
+            int l1=INT_MIN,l2=INT_MIN,r1=INT_MAX,r2=INT_MAX;
+            int mid2=leftTot-mid;
+            if(mid1>0)l1=nums1[mid1-1];
+            if(mid2>0)l2=nums2[mid2-1];
+            if(mid1<n1)r1=nums1[mid1];
+            if(mid2<n2)r2=nums2[mid2];
+            int leftmax=max(l1,l2);
+            int rightmin=min(r1,r2);
+            if(leftmax<=rightmin){
+                if((n1+n2)%2==0){
+                    return (leftmax + rightmin) / 2.0;
+
+                }
+                else{
+                    return leftmax;
+                }
+            }
+            else if(l1>r2){
+                high=mid-1;
             }
             else{
-                break;
+                low=mid+1;
             }
+
         }
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        double ans;
-        if((m+n)%2==0){
-            int ind1=(m+n)/2;
-            int ind2=ind1-1;
-            if(ind1<n){
-                ans=(double)(nums1[ind1]+nums1[ind2])/2.0;
-            }
-            else if(ind1>=n && ind2<n){
-                ans= (double)(nums2[ind1-n]+nums1[ind2])/2.0;
-            }
-            else{
-                ans= (double)(nums2[ind1-n]+nums2[ind2-n])/2.0;
-            }
-        }
-        else{
-            int ind=(m+n)/2;
-            if(ind<n)ans=nums1[ind];
-            else ans=nums2[ind-n];
-        }
-        return ans;
+        return 0;
     }
 };
