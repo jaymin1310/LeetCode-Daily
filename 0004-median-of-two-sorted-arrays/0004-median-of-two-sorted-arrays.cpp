0abe02bfc3f9aee84dd167d88cebe75bbc465c1b
jaymin1310 <jvsolanki2005@gmail.com>
2025-07-30 12:58:45 +0530

    Time: 0 ms (100%), Space: 95.1 MB (60.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -8,13 +8,13 @@ public:
         int leftTot=(n1+n2+1)/2;
         while(low<=high){
             int mid=(low+high)/2;
-            int mid1=mid;
+            int mid1=mid-1;
             int l1=INT_MIN,l2=INT_MIN,r1=INT_MAX,r2=INT_MAX;
-            int mid2=leftTot-mid;
-            if(mid1>0)l1=nums1[mid1-1];
-            if(mid2>0)l2=nums2[mid2-1];
-            if(mid1<n1)r1=nums1[mid1];
-            if(mid2<n2)r2=nums2[mid2];
+            int mid2=leftTot-mid-1;
+            if(mid1>=0)l1=nums1[mid1];
+            if(mid2>=0)l2=nums2[mid2];
+            if(mid1+1<n1)r1=nums1[mid1+1];
+            if(mid2+1<n2)r2=nums2[mid2+1];
             int leftmax=max(l1,l2);
             int rightmin=min(r1,r2);
             if(leftmax<=rightmin){
