fe6130c03ce31b68438a30cd3c276127908c3fd9
jaymin1310 <jvsolanki2005@gmail.com>
2025-08-01 00:33:18 +0530

    Time: 72 ms (19.56%), Space: 18.8 MB (36.61%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,6 +4,11 @@ public:
         int row=mat.size();
         int col=mat[0].size();
         for(int rowmid=0;rowmid<row;rowmid++){
+            if(mat[rowmid][0]>target){
+                break;
+            }
+            else if(mat[rowmid][0]==target)return true;
+            else{
             int low=0,high=col-1,mid;
             while(low<=high){
                 mid=(low+high)/2;
@@ -16,6 +21,7 @@ public:
                 else{
                     low=mid+1;
                 }
+            }
             }
         }
         return false;
