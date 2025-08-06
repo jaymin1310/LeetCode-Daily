9f68fb77bb473bc04b52f2abfe7e6a3eb6d8071d
jaymin1310 <jvsolanki2005@gmail.com>
2025-08-07 01:13:44 +0530

    Time: 8 ms (15.95%), Space: 49.2 MB (5.39%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        // Step 1: Trim leading and trailing spaces
-        while (left <= right && s[left] == ' ') left++;
-        while (right >= left && s[right] == ' ') right--;
-
-        string temp = "", ans = "";
-
-        while (left <= right) {
-            char ch = s[left];
-
-            if (ch != ' ') {
-                temp += ch;
-            } else {
-                // Skip multiple spaces
-                while (left <= right && s[left] == ' ') left++;
-
-                // If temp has a word, prepend it to ans
-                if (!temp.empty()) {
-                    if (!ans.empty()) ans = temp + " " + ans;
-                    else ans = temp;
-                    temp = "";
-                }
-
-                continue; // already moved left past space
+        int n=s.size();
+        reverse(s.begin(),s.end());
+        string word="",res="";
+        for(int i=0;i<n;i++){
+            if(s[i]!=' '){
+                word+=s[i];
             }
-            left++;
+            else{
+                if(!word.empty()){
+                    reverse(word.begin(),word.end());
+                    res=res+word+" ";
+                }
+                word="";
+            }
         }
-
-        // Append the last word
-        if (!temp.empty()) {
-            if (!ans.empty()) ans = temp + " " + ans;
-            else ans = temp;
+        if(!word.empty()){
+                    reverse(word.begin(),word.end());
+                    res+=word;
         }
-
-        return ans;
+        if(res.back()==' '){
+            res.pop_back();
+        }
+        return res;
     }
 };
