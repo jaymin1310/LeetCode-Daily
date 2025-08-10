d81a8b10a76636e3254ad5da6b6e3d198e6711bc
jaymin1310 <jvsolanki2005@gmail.com>
2025-08-10 23:56:26 +0530

    Time: 0 ms (100%), Space: 9.2 MB (93.89%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char>mp;
         int n=s.size();
-        if(t.size()!=n)return false;
+        if(n!=t.size()){
+            return false;
+        }
+        vector<int>st(256,-1);
+        vector<int>ts(256,-1);
         for(int i=0;i<n;i++){
-            auto it=mp.find(s[i]);
-            if(it!=mp.end()){
-                if(it->second!=t[i]){
-                    return false;
-                }
-                continue;
-            }
-            mp[s[i]]=t[i];
-        }
-        vector<char>check;
-        for(auto it:mp){
-            check.push_back(it.second);
-        }
-        sort(check.begin(),check.end());
-        for(int i=1;i<check.size();i++){
-            if(check[i]==check[i-1]){
+            unsigned char ch1=s[i];
+            unsigned char ch2=t[i];
+            if(st[ch1]!=-1 && st[ch1]!=ch2 || ts[ch2]!=-1 && ts[ch2]!=ch1){
                 return false;
             }
-        }
-        return true;
+            st[ch1]=ch2;
+            ts[ch2]=ch1;
+       }
+       return true;
     }
 };
