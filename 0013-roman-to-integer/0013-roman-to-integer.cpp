7caa6df37173bf292a2650ae1f1ce7f5b143f5f7
jaymin1310 <jvsolanki2005@gmail.com>
2025-08-11 23:50:41 +0530

    Time: 0 ms (100%), Space: 10.2 MB (77.54%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> val = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-            {'C', 100}, {'D', 500}, {'M', 1000}
+        int n = s.size();
+        if (n == 0) return 0;
+
+        // Helper function to get the value of a Roman character
+        auto getValue = [](char c) {
+            if (c == 'I') return 1;
+            if (c == 'V') return 5;
+            if (c == 'X') return 10;
+            if (c == 'L') return 50;
+            if (c == 'C') return 100;
+            if (c == 'D') return 500;
+            if (c == 'M') return 1000;
+            return 0;
         };
 
-        int result = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (i + 1 < s.size() && val[s[i]] < val[s[i + 1]])
-                result -= val[s[i]];
-            else
-                result += val[s[i]];
+        int result = getValue(s[0]);
+        int prev = result;
+
+        for (int i = 1; i < n; i++) {
+            int curr = getValue(s[i]);
+            if (curr > prev) {
+                result -= 2 * prev; // Subtract twice the previous value
+            }
+            result += curr;
+            prev = curr;
         }
+
         return result;
     }
 };
