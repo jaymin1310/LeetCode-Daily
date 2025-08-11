d5c99e49df440c68ac8fc6bf78a2a83194b2a739
jaymin1310 <jvsolanki2005@gmail.com>
2025-08-11 13:15:12 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,13 +4,14 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())return 0;
         vector<int>mp(26,0);
-        vector<int>mp2(26,0);
         for(int i=0;i<s.size();i++){
             mp[s[i]-'a']++;
-            mp2[t[i]-'a']++;
+            mp[t[i]-'a']--;
         }
-        if(mp==mp2)return 1;
-        return 0;
+        for(int cnt:mp){
+            if(cnt!=0)return 0;
+        }
+        return 1;
 
     }
 };
