2cdcb8222b3f9c4cdb23f1f28fb1d2a12e3f7bd9
jaymin1310 <jvsolanki2005@gmail.com>
2025-08-24 23:03:30 +0530

    Time: 90 ms (73.14%), Space: 11.3 MB (50.94%) - LeetHub

--- 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    int cntbeauty(string&s,int start,int end){
-        vector<int>arr(26,0);
-        int mincnt=INT_MAX,maxcnt=INT_MIN;
-        for(int i=start;i<=end;i++){
-            arr[s[i]-'a']++;
-        }
-        for(int i=0;i<26;i++){
-            if(arr[i]!=0){
-                mincnt=min(arr[i],mincnt);
-                maxcnt=max(arr[i],maxcnt);
-            }
-        }
-        return maxcnt-mincnt;
-    }
     int beautySum(string s) {
-        int n=s.size(),ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int res=cntbeauty(s,i,j);
-                ans+=res;
+        int n = s.size(), ans = 0;
+        for (int i = 0; i < n; i++) {
+            vector<int> arr(26, 0);
+            arr[s[i] - 'a']++;
+            for (int j = i + 1; j < n; j++) {
+                arr[s[j] - 'a']++;
+                int mincnt = INT_MAX, maxcnt = INT_MIN;
+                for (int i = 0; i < 26; i++) {
+                    if (arr[i] != 0) {
+                        mincnt = min(arr[i], mincnt);
+                        maxcnt = max(arr[i], maxcnt);
+                    }
+                }
+                ans+=(maxcnt-mincnt);
             }
         }
         return ans;
