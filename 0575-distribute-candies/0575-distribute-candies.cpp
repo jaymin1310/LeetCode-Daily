94d0c6ed37b3ab698cc270341ee2d7ab34c9d0c3
jaymin1310 <jvsolanki2005@gmail.com>
2025-09-13 23:49:34 +0530

    Time: 119 ms (38.37%), Space: 119.9 MB (74.56%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> types(candyType.begin(), candyType.end());
+        return min((int)types.size(), (int)candyType.size() / 2);
+    }
+};
