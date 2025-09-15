c24f51649a3d63707fd2f4a796b399d2a41a0e45
jaymin1310 <jvsolanki2005@gmail.com>
2025-09-15 22:23:41 +0530

    Time: 0 ms (100%), Space: 8.9 MB (97.41%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
+    int recursion(string &s,int i,int result,int sign,bool started){
+        if(i>=s.size()){
+            return sign*result;
+        }
+        if(!started && s[i]==' '){
+            return recursion(s,i+1,result,sign,started);
+        }
+        if(!started && ((s[i]=='-')||(s[i]=='+'))){
+            started=true;
+            sign=(s[i]=='-')?-1:1;
+            return recursion(s,i+1,result,sign,started);
+        }
+        if(isdigit(s[i])){
+            started=true;
+            int digit=s[i]-'0';
+            if((result>INT_MAX/10) || (result==INT_MAX/10 && digit>INT_MAX%10)){
+                return (sign==1)?INT_MAX:INT_MIN;
+            }
+            result=(result*10)+digit;
+            return recursion(s,i+1,result,sign,started);
+        }
+        return sign* result;
+    }
     int myAtoi(string s) {
-        int n=s.size();
-        long result=0;
-        int i=0,sign=1;
-            while(i<n && s[i]==' '){
-                i++;
-            }
-            if (i < n && (s[i] == '-' || s[i] == '+')) {
-                sign = (s[i] == '-') ? -1 : 1;
-                i++;
-            }
-            while(i<n && isdigit(s[i])){
-                int digit=s[i]-'0';
-                if((result>INT_MAX/10) || (result==INT_MAX/10 && digit>INT_MAX%10)){
-                    return sign==1?INT_MAX:INT_MIN;
-                }
-                result=result*10+digit;
-                i++;
-            }
-           return sign*result;
+        int result=0;
+        int sign=1;
+        return recursion(s,0,result,sign,false);
     }
 };
