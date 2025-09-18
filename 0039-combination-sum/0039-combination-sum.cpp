46aa1c7b7fa0760763e5b64e1c61f7927e2b2637
jaymin1310 <jvsolanki2005@gmail.com>
2025-09-18 22:18:35 +0530

    Time: 107 ms (5.08%), Space: 72.1 MB (8.38%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void recursion(vector<int>&nums,int k,vector<int>temp,vector<vector<int>>&ans,int i){
+        if(i>=nums.size() || k<=0){
+            if(k==0){
+                ans.push_back(temp);
+            }
+            return ;
+        }
+        temp.push_back(nums[i]);
+        recursion(nums,k-nums[i],temp,ans,i);
+        temp.pop_back();
+        recursion(nums,k,temp,ans,i+1);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        vector<vector<int>>ans;
+        recursion(candidates,target,temp,ans,0);
+        return ans;
+    }
+};
