49eed6d20ebe8e731a5a81f1f816aea8c11ff2fb
jaymin1310 <jvsolanki2005@gmail.com>
2025-09-21 23:14:46 +0530

    Time: 1 ms (20.29%), Space: 9.3 MB (38.82%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     unordered_map<char, string> mp = {{'2', "abc"},{'3', "def"},{'4', "ghi"},{'5', "jkl"},{'6', "mno"},{'7', "pqrs"},{'8', "tuv"},{'9', "wxyz"}};
     vector<string>ans;
-    void recursion(int i,string& digits,string temp){
+    void recursion(int i,string& digits,string& temp){
         if(i==digits.size()){
             ans.push_back(temp);
             return;
         }
         string get=mp[digits[i]];
         for(char c:get){
-            recursion(i+1,digits,temp+c);
+            temp[i] = c;  
+            recursion(i+1,digits,temp);
         }
     }
     vector<string> letterCombinations(string digits) {
         if (digits.empty()) return {};
-        recursion(0,digits,"");
+        string temp(digits.size(), ' '); 
+        recursion(0,digits,temp);
         return ans;
     }
 };
