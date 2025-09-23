604687b7bc7c82773f58bafb7c2a105b637433ae
jaymin1310 <jvsolanki2005@gmail.com>
2025-09-23 21:09:23 +0530

    Time: 126 ms (96.82%), Space: 10.5 MB (85.73%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int row;
+    int col;
+    bool recursion(vector<vector<char>> &board,string& word,int i,int j,int ind){
+        if(ind==word.size())return true;
+        char original = board[i][j];
+        board[i][j] = '#';
+        char temp=word[ind];
+        if(i<row && col>j+1 && board[i][j+1]==temp){
+            if(recursion(board,word,i,j+1,ind+1))return true;//right
+        }
+        if(j>0 && i<row && col>j-1 && board[i][j-1]==temp){
+            if(recursion(board,word,i,j-1,ind+1))return true;;//left
+        }
+        if(i>0 && i-1<row && col>j && board[i-1][j]==temp){
+            if(recursion(board,word,i-1,j,ind+1))return true;//up
+        }
+        if(i+1<row && col>j && board[i+1][j]==temp){
+            if(recursion(board,word,i+1,j,ind+1))return true;//down
+        }
+        board[i][j]=original;
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int r=board.size();
+        int c=board[0].size();
+        row=r;
+        col=c;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(board[i][j]==word[0]){
+                    if(recursion(board,word,i,j,1)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
