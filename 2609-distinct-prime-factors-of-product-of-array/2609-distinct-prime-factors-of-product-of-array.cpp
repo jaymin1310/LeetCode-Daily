f7a1ab2e20317de4a005a299edb14f1ed0c683c0
jaymin1310 <jvsolanki2005@gmail.com>
2025-10-07 12:36:11 +0530

    Time: 8 ms (75.25%), Space: 23.2 MB (17.21%) - LeetHub

--- 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.cpp
+++ 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    void helper(int N,unordered_set<int>&ans, vector<int>&spf){
-         while(N!=1){
+    void helper(int N, unordered_set<int>& ans, vector<int>& spf) {
+        while (N != 1) {
             ans.insert(spf[N]);
-            N/=spf[N];
+            N /= spf[N];
         }
     }
+    
     int distinctPrimeFactors(vector<int>& nums) {
         int maxN = *max_element(nums.begin(), nums.end());
-        vector<int>spf(maxN+1,1);
-        for(int i=2;i<=maxN;i++){
-            if(spf[i]==1){
-                spf[i]=i;
-                for(int j=i*i;j<=maxN;j+=i){
-                    spf[j]=i;
-                }
+        vector<int> spf(maxN + 1, 1);
+        for (int i = 2; i <= maxN; i++) {
+            if (spf[i] == 1) {
+                spf[i] = i;
+                for (int j = i * i; j <= maxN; j += i)
+                    spf[j] = i;
             }
         }
-        unordered_set<int>ans;
-        for(int i=0;i<nums.size();i++){
-            helper(nums[i],ans,spf);
-        }
+        unordered_set<int> ans;
+        for (int num : nums)
+            helper(num, ans, spf);
         return ans.size();
     }
 };
