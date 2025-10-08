25cc06d9ba8e719baf4a59073086bd59bf9ba737
jaymin1310 <jvsolanki2005@gmail.com>
2025-10-08 23:32:50 +0530

    Time: 0 ms (100%), Space: 24.6 MB (47.49%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int count = 0;
+        for (int h : hours) {
+            if (h >= target) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
