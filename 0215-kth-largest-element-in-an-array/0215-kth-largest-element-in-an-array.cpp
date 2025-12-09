4c643c1b7d7ba69ce4f44b29523d93fd57ed255c
jaymin1310 <jvsolanki2005@gmail.com>
2025-12-10 00:14:51 +0530

    Time: 2744 ms (5.01%), Space: 59.2 MB (93.57%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        for(int x:nums){
-            pq.push(x);
-            if(pq.size()>k)pq.pop();
+    int findIndex(vector<int>& nums, int start, int end) {
+        int pivot = nums[start];
+        int low = start + 1;
+        int high = end;
+        while (low <= high) {
+            while (low <= high && nums[low] > pivot)
+                low++;
+            while (low <= high && nums[high] <= pivot)
+                high--;
+            if (low < high) {
+                swap(nums[low], nums[high]);
+            }
         }
-        return pq.top();
+        swap(nums[start], nums[high]);
+        return high;
+    }
+    int partation(vector<int>& nums, int start, int end, int k) {
+        int pind = findIndex(nums, start, end);
+        if (pind + 1 == k)
+            return nums[pind];
+        else if (pind >= k) {
+            end = pind - 1;
+        } else {
+            start = pind + 1;
+        }
+        return partation(nums, start, end, k);
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        return partation(nums, 0, nums.size() - 1, k);
     }
 };
