5c879605ee99de867282a99096e8c3f902248907
jaymin1310 <jvsolanki2005@gmail.com>
2025-12-14 01:00:41 +0530

    Time: 3 ms (73.54%), Space: 38.2 MB (82.37%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-          vector<int>freq(26,0);
+         vector<int>freq(26,0);
+          int maxcnt=0;
           for(int i=0;i<tasks.size();i++){
             freq[tasks[i]-'A']++;
           }
-          priority_queue<pair<int,int>>pq;
+          int maxFreq = *max_element(freq.begin(), freq.end());
           for(int i=0;i<26;i++){
-            if(freq[i]>0){
-                pq.push({freq[i],i});
+            if(freq[i]==maxFreq){
+                maxcnt++;
             }
           }
-          int ans=0;
-          queue<pair<int,int>>cool;
-        while(true){
-            int tot=pq.size();
-            int cnt=0;
-            while(!pq.empty() && cnt<n+1){
-                auto it=pq.top();
-                pq.pop();
-                it.first--;
-                if(it.first>0)cool.push(it);
-                cnt++;
-            }
-            if(cool.empty()){
-                ans+=tot;
-                break;
-            }
-            ans+=n+1;
-            while(!cool.empty()){
-                pq.push(cool.front());
-                cool.pop();
-            }
-        }
-        return ans;
+         int ans= max((int)tasks.size(),(maxFreq - 1) * (n + 1) + maxcnt);
+         return ans;
     }
 };
