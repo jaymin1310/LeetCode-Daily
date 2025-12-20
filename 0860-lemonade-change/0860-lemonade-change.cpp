cff36536e4e8caee52b0cb03ff11bee14c746bde
jaymin1310 <jvsolanki2005@gmail.com>
2025-12-20 23:36:34 +0530

    Time: 0 ms (100%), Space: 87.2 MB (71.07%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        unordered_map<int,int>mp;
-        int n=bills.size();
-        for(int i=0;i<n;i++){
-            if(bills[i]==10){
-                if(mp[5]==0)return false;
-                mp[5]--;
+        unordered_map<int,int> mp;
+        for (int b : bills) {
+            if (b == 5) {
+                mp[5]++;
+                continue;
             }
-            else if(bills[i]==20){
-                if(mp[10]>0 && mp[5]>0){
+
+            if (b == 10) {
+                if (mp[5] == 0) return false;
+                mp[5]--;
+                mp[10]++;
+            }
+
+            else { // b == 20
+                if (mp[10] > 0 && mp[5] > 0) {
                     mp[10]--;
                     mp[5]--;
-                }else{
-                    if(mp[5]>=3){
-                        mp[5]-=3;
-                        continue;
-                    }
+                } 
+                else if (mp[5] >= 3) {
+                    mp[5] -= 3;
+                } 
+                else {
                     return false;
                 }
             }
-            mp[bills[i]]++;
-        }   
+        }
         return true;
     }
 };
