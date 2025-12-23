3bd86d887125ccc991a7bf41a863d8747aeabe46
jaymin1310 <jvsolanki2005@gmail.com>
2025-12-23 23:08:40 +0530

    Time: 0 ms (100%), Space: 52.3 MB (65.77%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0,i=0,nexi=nums[0];
-        i++;
-        if(nexi>=n-1)return true;
-        while(i<n){ 
-            int maxi=0,curmax=0;
-            while(i<n && i<=nexi){
-                if(i+nums[i] > curmax){
-                    curmax=i+nums[i];
-                    maxi=i;
-                }
-                i++;
-            }
-            if(maxi>=n-1)return true;
-            if(maxi+nums[maxi]<=nexi)return false;
-            nexi=maxi+nums[maxi];
+        int n=nums.size(),maxind=0;
+        for(int i=0;i<n;i++){
+            if(i>maxind)return false;
+            maxind=max(maxind,i+nums[i]);
+            if(maxind>=n-1)return true;
         }
         return true;
     }
