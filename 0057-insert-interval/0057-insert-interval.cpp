cf3b023459848b51b03082215fee8527eb9a48b2
jaymin1310 <jvsolanki2005@gmail.com>
2025-12-28 02:18:39 +0530

    Time: 0 ms (100%), Space: 21.7 MB (72.98%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -8,42 +8,16 @@ public:
         vector<vector<int>> res;
 
         // Add intervals that come before newInterval
-        while (i < n && intervals[i][0] < newInterval[0]) {
+        while (i < n && intervals[i][1] < newInterval[0]) {
             res.push_back(intervals[i]);
             i++;
         }
-
-        // Case: newInterval goes after all and no overlap
-        if (i >= n && !res.empty() && res.back()[1] < newInterval[0]) {
-            res.push_back(newInterval);
-            return res;
-        }
-
-        // Merge with last interval in res if needed
-        if (!res.empty()) {
-            int lastStart = res.back()[0];
-            int lastEnd = res.back()[1];
-            if (lastEnd >= newInterval[0]) {
-                res.pop_back();
-                newintStart = min(lastStart, newInterval[0]);
-                newintLast = max(lastEnd, newInterval[1]);
-            } else {
-                newintStart = newInterval[0];
-                newintLast = newInterval[1];
-            }
-        } else {
-            newintStart = newInterval[0];
-            newintLast = newInterval[1];
-        }
-
-        // FIXED: merge overlapping intervals
-        while (i < n && intervals[i][0] <= newintLast) {
-            newintLast = max(newintLast, intervals[i][1]);
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
             i++;
         }
-
-        res.push_back({newintStart, newintLast});
-
+        res.push_back(newInterval);
         // Push remaining intervals
         while (i < n) {
             res.push_back(intervals[i]);
