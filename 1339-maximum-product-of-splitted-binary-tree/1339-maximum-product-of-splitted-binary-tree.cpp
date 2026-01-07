6c400ddc886619028a64998f5234f2edbe1a92ca
jaymin1310 <jvsolanki2005@gmail.com>
2026-01-08 00:55:03 +0530

    Time: 4 ms (65.02%), Space: 80.1 MB (22.77%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long mul=1;
+    long long recursion(TreeNode* root,long long sum){
+        if(root==nullptr)return 0;
+        long long temp= recursion(root->left,sum)+
+        recursion(root->right,sum)+root->val;
+        mul=max(mul,temp*(sum-temp));
+        return temp;
+
+    }
+    int maxProduct(TreeNode* root) {
+        long long sum=0;
+        int MOD=1e9+7;
+        if(!root)return 0;
+        queue<TreeNode*>qu;
+        qu.push(root);
+        while(!qu.empty()){
+            TreeNode* front=qu.front();
+            sum+=front->val;
+            qu.pop();
+            if(front->left)qu.push(front->left);
+            if(front->right)qu.push(front->right);
+        }
+        recursion(root,sum);
+        return mul%MOD;
+    }
+};
