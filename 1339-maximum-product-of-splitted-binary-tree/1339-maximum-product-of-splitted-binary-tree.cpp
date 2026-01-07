3b963cb4096fb9b1231215f24b1afe0fe88b6010
jaymin1310 <jvsolanki2005@gmail.com>
2026-01-08 01:05:19 +0530

    Time: 0 ms (100%), Space: 78 MB (73.24%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,39 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    long long mul=1;
-    long long recursion(TreeNode* root,long long sum){
-        if(root==nullptr)return 0;
-        long long left=recursion(root->left,sum);
-        long long right=recursion(root->right,sum);
-        long long tempsum=left+right+root->val;
-        mul=max(mul,tempsum*(sum-tempsum));
-        return tempsum;
+    long long maxp; 
+    long long total;
+
+    long long sum(TreeNode* node) {
+        if (node == nullptr) return 0;
+        return sum(node->left) + sum(node->right) + node->val;
     }
+
+    long long dfs(TreeNode* root) {
+        if (root == nullptr) return 0;
+        auto ssum = root->val + dfs(root->left) + dfs(root->right);
+        maxp = max(maxp, ssum * (total - ssum));
+        return ssum;
+    }
+
     int maxProduct(TreeNode* root) {
-        long long sum=0;
-        int MOD=1e9+7;
-        if(!root)return 0;
-        queue<TreeNode*>qu;
-        qu.push(root);
-        while(!qu.empty()){
-            TreeNode* front=qu.front();
-            sum+=front->val;
-            qu.pop();
-            if(front->left)qu.push(front->left);
-            if(front->right)qu.push(front->right);
-        }
-        recursion(root,sum);
-        return mul%MOD;
+        maxp = 0;
+        total = sum(root);
+        dfs(root);
+        long long MOD = 1000000007LL;
+        return int(maxp % MOD);
     }
 };
