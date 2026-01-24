e818f7c15bca01d31dc3ccdf46d254f3557988db
jaymin1310 <jvsolanki2005@gmail.com>
2026-01-24 21:48:07 +0530

    Time: 0 ms (100%), Space: 27.7 MB (19.19%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,36 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    unordered_map<int,int>mp;
-    int traverse=0;
-    TreeNode* recursion(vector<int>&preorder,int low,int high){
-        if(low>high)return nullptr;
-        int val=preorder[traverse];
-        traverse++;
-        TreeNode* temp=new TreeNode(val);
-        int ind=mp[val];
-        if(low==high){
-            return temp;
-        }
-        temp->left=recursion(preorder,low,ind-1);
-        temp->right=recursion(preorder,ind+1,high);
-        return temp;
+    TreeNode* build(vector<int>& preorder, int low, int high,
+                    unordered_map<int,int>& mp, int& idx) {
+        if (low > high) return nullptr;
+
+        int val = preorder[idx++];
+        TreeNode* root = new TreeNode(val);
+
+        int mid = mp[val];
+
+        root->left = build(preorder, low, mid - 1, mp, idx);
+        root->right = build(preorder, mid + 1, high, mp, idx);
+
+        return root;
     }
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n=inorder.size();
-        for(int i=0;i<n;i++){
-            mp[inorder[i]]=i;
-        }
-        return recursion(preorder,0,n-1);
+        unordered_map<int,int> mp;
+        for (int i = 0; i < inorder.size(); i++)
+            mp[inorder[i]] = i;
+
+        int idx = 0;
+        return build(preorder, 0, inorder.size() - 1, mp, idx);
     }
 };
