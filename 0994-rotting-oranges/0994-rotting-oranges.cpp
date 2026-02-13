3fff85c2bf8d6f5a25b08272e08a3f42c350385e
jaymin1310 <jvsolanki2005@gmail.com>
2026-02-14 00:41:45 +0530

    Time: 0 ms (100%), Space: 16.5 MB (97.05%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,41 +2,51 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>adj(m*n);
-        vector<int>visited(m*n,0);
-        queue<int>qu;
+        queue<pair<int,int>>qu;
         int cnt=0;
-        int isOrange=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 int v=i*n+j;
-                if(grid[i][j]==2)qu.push(v);
-                else if(grid[i][j]==0)continue;
-                if(j+1<n && grid[i][j+1])adj[v].push_back(v+1);
-                if(j-1>=0 && grid[i][j-1])adj[v].push_back(v-1);
-                if(i-1>=0 && grid[i-1][j])adj[v].push_back(v-n);
-                if(i+1<m && grid[i+1][j])adj[v].push_back(v+n);
-                isOrange=1;
-                cnt++;
+                if(grid[i][j]==2)qu.push({i,j});
+                else if(grid[i][j]==1)cnt++;
             }
         }
-        if(!isOrange)return 0;
-        int minute=-1;
+        int minute=0;
         while(!qu.empty()){
             int size=qu.size();
-            int flag=0;
+            bool rottedThisMinute = false;
             while(size--){
-                int front=qu.front();
+                auto it=qu.front();
                 qu.pop();
-                if(visited[front])continue;
-                visited[front]=1;
-                for(auto it:adj[front]){
-                    qu.push(it);
+                int i=it.first,j=it.second;
+                if(j+1<n && grid[i][j+1]==1){
+                    grid[i][j+1]=2;
+                    qu.push({i,j+1});
+                    cnt--;
+                    rottedThisMinute = true;
                 }
-                flag=1;
-                cnt--;
+                if(j-1>=0 && grid[i][j-1]==1){
+                    grid[i][j-1]=2;
+                    qu.push({i,j-1});
+                    cnt--;
+                    rottedThisMinute = true;
+                }
+                if(i+1<m && grid[i+1][j]==1){
+                    grid[i+1][j]=2;
+                    qu.push({i+1,j});
+                    cnt--;
+                    rottedThisMinute = true;
+                }
+                if(i-1>=0 && grid[i-1][j]==1){
+                    grid[i-1][j]=2;
+                    qu.push({i-1,j});
+                    cnt--;
+                    rottedThisMinute = true;
+                }
             }
-            if(flag==1)minute++;
+            if(rottedThisMinute)
+                minute++;
+
         }
         return (!cnt)?minute:-1;
     }
