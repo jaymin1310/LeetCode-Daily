1a1d3af5af8f885738f50330af151e78ca30b7dd
jaymin1310 <jvsolanki2005@gmail.com>
2026-02-14 22:47:01 +0530

    Time: 0 ms (100%), Space: 10.1 MB (75.54%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+    double champagneTower(int poured,int query_row,int query_glass){
+        vector<double>dp(query_row+2,0.0);
+        dp[0]=poured;
+        for(int r=0;r<query_row;r++){
+            for(int c=r;c>=0;c--){
+                double overflow=max(0.0,(dp[c]-1)/2);
+                dp[c]=overflow;
+                dp[c+1]+=overflow;
+            }
+        }
+        return min(1.0,dp[query_glass]);
+    }
+};
