195c74cbc4cc3d8ec91fd10b04dba3cd2e569fe5
jaymin1310 <jvsolanki2005@gmail.com>
2026-02-26 00:34:22 +0530

    Time: 0 ms (100%), Space: 14.2 MB (79.86%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    int helper(int n){
-        int cnt=0;
-        while(n){
-            if(n&1)cnt++;
-            n=n>>1;
-        }
-        return cnt;
+    static bool compare(int a,int b){
+        int cnta=__builtin_popcount(a);
+        int cntb=__builtin_popcount(b);
+        if(cnta==cntb)return a<b;
+        return cnta<cntb;
     }
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        vector<int>ans(arr.size());
-        vector<int>count(15,0);
-        for(int i=0;i<arr.size();i++){
-            count[helper(arr[i])]++;
-        }
-        for(int i=1;i<15;i++){
-            count[i]+=count[i-1];
-        }
-        for(int i=arr.size()-1;i>=0;i--){
-            int bitcnt=helper(arr[i]);
-            ans[count[bitcnt]-1]=arr[i];
-            count[bitcnt]--;
-        }
-        return ans;
+        sort(arr.begin(),arr.end(),compare);
+        return arr;
     }
 };
