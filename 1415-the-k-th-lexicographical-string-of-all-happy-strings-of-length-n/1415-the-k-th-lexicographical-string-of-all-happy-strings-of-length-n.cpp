763075237f5a41c7df298302e665d03573011d7d
jaymin1310 <jvsolanki2005@gmail.com>
2026-03-14 22:45:23 +0530

    Time: 0 ms (100%), Space: 7.8 MB (96.74%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    string helper(int i,int n,int k,string temp,int &cnt,char last){
-        if(i>=n){
-            cnt++;
-            if(cnt==k){
-                return temp;
-            }
-            else{
-                return "";
-            }
-
-        }
-        string a,b,c;
-        if(last!='a')a=helper(i+1,n,k,temp+'a',cnt,'a');
-        if(last!='b')b=helper(i+1,n,k,temp+'b',cnt,'b');
-        if(last!='c')c=helper(i+1,n,k,temp+'c',cnt,'c');
-        if(a!="")return a;
-        else if(b!="")return b;
-        return c;
-    }
-
     string getHappyString(int n, int k) {
-        int cnt=0;
-        return helper(0,n,k,"",cnt,'\0');
+
+        int total = 3 * (1 << (n-1));
+        if(k > total) return "";
+
+        string ans = "";
+        char last = '#';
+
+        for(int i=0;i<n;i++){
+            for(char c : {'a','b','c'}){
+
+                if(c == last) continue;
+
+                int rem = n - i - 1;
+                int count = 1 << rem;
+
+                if(k > count){
+                    k -= count;
+                }
+                else{
+                    ans += c;
+                    last = c;
+                    break;
+                }
+            }
+        }
+
+        return ans;
     }
 };
