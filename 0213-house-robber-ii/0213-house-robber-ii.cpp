ccfa5df82c5e041766a15b6a102595b038cdc203
jaymin1310 <jvsolanki2005@gmail.com>
2026-04-21 23:51:23 +0530

    Time: 0 ms (100%), Space: 10.3 MB (61.26%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return nums[0];
-        if(n==2)return max(nums[0],nums[1]);
-        vector<int>dp(n,-1);
-        vector<int>dp1(n,-1);
-        dp[0]=nums[0];
-        dp1[0]=0;
-        dp[1]=max(nums[1],nums[0]);
-        dp1[1]=nums[1];
-        for(int i=2;i<n-1;i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-            dp1[i]=max(dp1[i-1],dp1[i-2]+nums[i]);
+    int robinghouse(vector<int>& nums,int l,int r) {
+        int prev1=0,prev2=0;
+        for(int i=l;i<=r;i++){
+            int curr=max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=curr;
         }
-        dp[n-1]=dp[n-2];
-        dp1[n-1]=max(dp1[n-2],dp1[n-3]+nums[n-1]);
-        return max(dp[n-1],dp1[n-1]);
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        return max(robinghouse(nums,0,n-2),robinghouse(nums,1,n-1));
     }
 };
