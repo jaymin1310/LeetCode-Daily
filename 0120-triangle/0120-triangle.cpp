0772aaacab417faffa1e6adaff5c8256b823ff53
jaymin1310 <jvsolanki2005@gmail.com>
2026-04-25 00:07:18 +0530

    Time: 0 ms (100%), Space: 12.7 MB (66.89%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,13 +4,11 @@ public:
         int n=triangle.size();
         vector<int>dp(n,0);
         for(int i=0;i<n;i++){
-            vector<int>temp(n,0);
-            for(int j=0;j<=i;j++){
-                if(j==0)temp[j]=dp[j]+triangle[i][j];
-                else if(j==i)temp[j]=dp[j-1]+triangle[i][j];
-                else temp[j]=min(triangle[i][j]+dp[j],triangle[i][j]+dp[j-1]);
+            for(int j=i;j>=0;j--){
+                if(j==0)dp[j]=dp[j]+triangle[i][j];
+                else if(j==i)dp[j]=dp[j-1]+triangle[i][j];
+                else dp[j]=min(dp[j],dp[j-1])+triangle[i][j];
             }
-            dp=temp;
         }
         int minsum=INT_MAX;
         for(int j=0;j<triangle.size();j++){
