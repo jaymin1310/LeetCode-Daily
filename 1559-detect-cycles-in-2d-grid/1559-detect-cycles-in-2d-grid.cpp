3e5efc553a9adacfc5d3ae61925d9eab2fb662a7
jaymin1310 <jvsolanki2005@gmail.com>
2026-04-27 01:28:21 +0530

    Time: 1105 ms (5.04%), Space: 365.3 MB (5.04%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>&grid,vector<vector<int>>&vis,int i,int j,pair<int,int>par){
-        pair<int,int>newpar={i,j};
-        if(vis[i][j])return true;
-        vis[i][j]=1;
-        vector<vector<int>>dir={{0,-1},{0,1},{1,0},{-1,0}};
-        for(auto &it:dir){
-            int nrow=i+it[0];
-            int ncol=j+it[1];
-            pair<int,int>temppar={nrow,ncol};
-            if(nrow>=0 && ncol>=0 && nrow<grid.size() && ncol<grid[0].size() && par!=temppar && grid[nrow][ncol]==grid[i][j]){
-                if(dfs(grid,vis,nrow,ncol,newpar)){
+    bool dfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j,
+             pair<int, int> par) {
+        vis[i][j] = 1;
+        vector<vector<int>> dir = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
+        for (auto& it : dir) {
+            int nrow = i + it[0];
+            int ncol = j + it[1];
+            if (nrow >= 0 && ncol >= 0 && nrow < grid.size() &&
+                ncol < grid[0].size() && grid[nrow][ncol] == grid[i][j]) {
+
+                if (!vis[nrow][ncol]) {
+                    if (dfs(grid, vis, nrow, ncol, {i, j}))
+                        return true;
+                } else if (make_pair(nrow, ncol) != par) {
                     return true;
                 }
             }
@@ -18,11 +21,12 @@ public:
         return false;
     }
     bool containsCycle(vector<vector<char>>& grid) {
-        vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(!vis[i][j]){
-                    if(dfs(grid,vis,i,j,{-1,-1}))return true;
+        vector<vector<int>> vis(grid.size(), vector<int>(grid[0].size(), 0));
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (!vis[i][j]) {
+                    if (dfs(grid, vis, i, j, {-1, -1}))
+                        return true;
                 }
             }
         }
