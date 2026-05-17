0f0fc13d8063ec0e46623af0ae1bca702d0d2afb
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-05-18 00:04:56 +0530

    Time: 9 ms (49.73%), Space: 44.9 MB (10.83%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    int recursion(vector<int>&g,vector<int>&s,int gind,int sind){
-        if(gind>=g.size() || sind>=s.size())return 0;
-        int res=0;
-        if(g[gind]>s[sind]){
-            res+=recursion(g,s,gind,sind+1);
-        }else{
-            res=res+1+recursion(g,s,gind+1,sind+1);
-        }
-        return res;
-    }
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        return recursion(g,s,0,0);
+        int i=0,j=0,cnt=0;
+        while(i<g.size() && j<s.size()){
+            if(g[i]<=s[j]){
+                cnt++;
+                i++;
+            }
+            j++;
+        }
+        return cnt;
     }
 };
