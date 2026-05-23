e8723345cf6daffbbe8ce4288762079b1984fcff
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-05-23 22:20:23 +0530

    Time: 0 ms (100%), Space: 11.4 MB (18.08%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int maxLeft=-1;
-        int smallLeft=nums[0];
         for(int i=1;i<nums.size();i++){
             if(nums[i-1]>nums[i]){
                 if(maxLeft!=-1)return false;
                 maxLeft=nums[i-1];
             }
-            if(maxLeft!=-1 && (nums[i]>maxLeft || nums[i]>smallLeft) )return false;
+            if(maxLeft!=-1 && (nums[i]>maxLeft || nums[i]>nums[0]) )return false;
         }
         return true;
     }
