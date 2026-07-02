3b00a228aac819fde81804ee6a7efb7557778704
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-07-03 01:44:22 +0530

    Time: 14 ms (74.36%), Space: 32.8 MB (43.74%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,33 +1,41 @@
 class Solution {
-    int const r[4] = {-1, 1, 0, 0};
-    int const c[4] = {0, 0, 1, -1};
+    int const dr[4] = {-1, 1, 0, 0};
+    int const dc[4] = {0, 0, 1, -1};
 
 public:
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size();
         int n = grid[0].size();
+
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            qu;
-        qu.push({grid[0][0], {0, 0}});
+        deque<pair<int, int>> dq;
+
         dist[0][0] = grid[0][0];
-        while (!qu.empty()) {
-            auto [dmg, cell] = qu.top();
-            auto [row, col] = cell;
-            qu.pop();
-            for (int i = 0; i < 4; i++) {
-                int nr = r[i] + row;
-                int nc = c[i] + col;
-                if (nr >= 0 && nc >= 0 && nr < m && nc < n) {
-                    if (dist[nr][nc] > dmg + grid[nr][nc]) {
-                        dist[nr][nc] = dmg + grid[nr][nc];
-                        qu.push({dist[nr][nc],{nr, nc}});
+        dq.push_front({0, 0});
+
+        while (!dq.empty()) {
+            auto [row, col] = dq.front();
+            dq.pop_front();
+
+            for (int k = 0; k < 4; k++) {
+                int nr = row + dr[k];
+                int nc = col + dc[k];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                    int newDamage = dist[row][col] + grid[nr][nc];
+
+                    if (newDamage < dist[nr][nc]) {
+                        dist[nr][nc] = newDamage;
+
+                        if (grid[nr][nc] == 0)
+                            dq.push_front({nr, nc}); // cost 0
+                        else
+                            dq.push_back({nr, nc});  // cost 1
                     }
                 }
             }
         }
+
         return dist[m - 1][n - 1] < health;
     }
 };
