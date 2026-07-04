9f5b5fae1591c620469479635afe3b4a9fdd920c
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-07-05 00:43:16 +0530

    Time: 64 ms (52.85%), Space: 134 MB (62.35%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,48 +1,34 @@
-class DSU {
-    vector<int> parent, size;
-
-public:
-    DSU(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-    int findUpar(int u) {
-        if (parent[u] == u)
-            return u;
-        return parent[u] = findUpar(parent[u]);
-    }
-    void unionBySize(int u, int v) {
-        int uu = findUpar(u);
-        int uv = findUpar(v);
-        // if both in same componant from initially
-        if (uu == uv)
-            return;
-        if (size[uu] >= size[uv]) {
-            parent[uv] = uu;
-            size[uu] += size[uv];
-        } else {
-            parent[uu] = uv;
-            size[uv] += size[uu];
-        }
-    }
-};
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        DSU obj=DSU(n+1);
-        int ans=INT_MAX;
-        for(auto &it:roads){
-            obj.unionBySize(it[0],it[1]);
+        vector<vector<pair<int,int>>> adj(n + 1);
+
+        for (auto &r : roads) {
+            adj[r[0]].push_back({r[1], r[2]});
+            adj[r[1]].push_back({r[0], r[2]});
         }
-        int mainPar=obj.findUpar(1);
-        for(auto &it:roads){
-            if(obj.findUpar(it[0])==mainPar){
-                ans=min(it[2],ans);
+
+        vector<int> vis(n + 1, 0);
+        queue<int> q;
+        q.push(1);
+        vis[1] = 1;
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for (auto &[nbr, wt] : adj[node]) {
+                ans = min(ans, wt);
+
+                if (!vis[nbr]) {
+                    vis[nbr] = 1;
+                    q.push(nbr);
+                }
             }
         }
+
         return ans;
     }
 };
