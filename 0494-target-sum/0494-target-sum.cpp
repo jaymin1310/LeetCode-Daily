c668223809cd994fd831a5e4ca72ad070947102d
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-07-19 23:16:10 +0530

    Time: 0 ms (100%), Space: 14.9 MB (60.15%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        // Impossible cases
+        if (abs(target) > totalSum || (totalSum + target) % 2 != 0)
+            return 0;
+
+        int K = (totalSum + target) / 2;
+        int n = nums.size();
+
+        vector<int> dp(K + 1, 0);
+
+        // Base case
+        if (nums[0] == 0)
+            dp[0] = 2;          // +0 and -0
+        else {
+            dp[0] = 1;
+            if (nums[0] <= K)
+                dp[nums[0]] = 1;
         }
-        vector<int>dp(2*sum+1,0);
-        if(target >= -sum && target <= sum)
-            dp[target + sum] = 1;
-        for(int i=n-1;i>=0;i--){
-            vector<int>temp(2*sum+1,0);
-            for(int currsum=-sum;currsum<=sum;currsum++){
-                int ways=0;
-                if(currsum-nums[i]>=-sum){
-                    ways+=dp[currsum-nums[i]+sum];
-                }
-                if(currsum+nums[i]<=sum){
-                    ways+=dp[currsum+nums[i]+sum];
-                }
-                temp[currsum+sum]=ways;
+
+        for (int i = 1; i < n; i++) {
+            vector<int> curr(K + 1, 0);
+
+            for (int sum = 0; sum <= K; sum++) {
+                int notTake = dp[sum];
+                int take = 0;
+
+                if (nums[i] <= sum)
+                    take = dp[sum - nums[i]];
+
+                curr[sum] = take + notTake;
             }
-            dp=temp;
+
+            dp = curr;
         }
-        return dp[sum];
+
+        return dp[K];
     }
 };
