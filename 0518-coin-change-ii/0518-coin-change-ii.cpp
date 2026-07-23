d30070dc092eb760e5c9c4dbcee9e88ee20681c8
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-07-23 22:27:27 +0530

    Time: 49 ms (18.59%), Space: 61.2 MB (14.75%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int helper(int amt,vector<int>&coins,int i,vector<vector<int>>&dp){
-        if(i==coins.size()){
-            return amt==0?1:0;
+    int change(int target, vector<int>& coins) {
+        vector<vector<long long>>dp(coins.size(),vector<long long>(target+1,0));
+        int n=coins.size();
+        for(int i=0;i<=target;i++){
+            if(i%coins[0]==0)dp[0][i]=1;
         }
-        if(amt<0)return 0;
-        if(dp[i][amt]!=-1)return dp[i][amt];
-        int take=helper(amt-coins[i],coins,i,dp);
-        int nottake=helper(amt,coins,i+1,dp);
-        return dp[i][amt]=take+nottake;
-
-    }
-    int change(int amount, vector<int>& coins) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-        return helper(amount,coins,0,dp);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+                int take=0;
+                if(j-coins[i]>=0)
+                take=dp[i][j-coins[i]];
+                long long nottake=dp[i-1][j];
+                dp[i][j]=1LL*(take+nottake);
+            }
+        }
+        return dp[n-1][target];
     }
 };
