21b6c5c54ceda3aca979c5a77a299a6bf56fbd59
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-08-11 00:56:00 +0530

    Time: 15 ms (95.36%), Space: 10.9 MB (69.73%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool isPos(int n,int rem,vector<int>&dp){
-        if(rem==0)return false;
-        if(dp[rem]!=-1)return dp[rem];
-        for(int i=1;i*i<=rem;i++){
-            int sq=i*i;
-            if(!isPos(n,rem-sq,dp))
-                return dp[rem]=true;
-        }
-        return dp[rem]=false;
-    }
     bool winnerSquareGame(int n) {
-        vector<int>dp(n+1,-1);
-        return isPos(n,n,dp);
+        vector<int> dp(n + 1, false);
+        dp[0] = false;
+        for (int rem = 1; rem <= n; rem++) {
+            for (int i = 1; i * i <= rem; i++) {
+                int sq = i * i;
+                if (!dp[rem - sq]){
+                    dp[rem] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 };
