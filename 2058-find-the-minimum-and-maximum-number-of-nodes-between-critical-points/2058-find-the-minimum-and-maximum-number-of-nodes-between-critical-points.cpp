fbeaa2ad401253bfb2b15c1e3b3c6ca8c8f17713
Jaymin Solanki <jvsolanki2005@gmail.com>
2026-08-31 20:22:57 +0530

    Time: 0 ms (100%), Space: 124.6 MB (60.57%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,41 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         ListNode* prev = head;
-        ListNode* curr = (head->next) ? head->next : nullptr;
-        ListNode* nextn = (curr) ? curr->next : nullptr;
-        if (!nextn)
-            return {-1, -1};
-        int lower = -1, higher = -1;
-        int cnt = 1, mindist = INT_MAX;
+        ListNode* curr = head->next;
+        ListNode* nextn = curr->next;
+
+        int first = -1;
+        int last = -1;
+        int cnt = 1;
+        int mindist = INT_MAX;
+
         while (nextn) {
-            if (((curr->val > prev->val) && (curr->val > nextn->val)) ||
-                ((curr->val < prev->val) && (curr->val < nextn->val))) {
-                cout << cnt << " ";
-                if (lower == -1)
-                    lower = cnt;
-                if (higher != -1) {
-                    mindist = min(mindist, cnt - higher);
+
+            if ((curr->val > prev->val && curr->val > nextn->val) ||
+                (curr->val < prev->val && curr->val < nextn->val)) {
+
+                if (first == -1) {
+                    first = cnt;
                 }
-                higher = cnt;
+
+                if (last != -1) {
+                    mindist = min(mindist, cnt - last);
+                }
+
+                last = cnt;
             }
+
             cnt++;
             prev = curr;
             curr = nextn;
             nextn = nextn->next;
         }
+
         if (mindist == INT_MAX)
             return {-1, -1};
-        return {mindist, higher - lower};
+
+        return {mindist, last - first};
     }
 };
